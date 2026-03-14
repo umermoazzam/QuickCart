c71fc769769746690e7c65896049866ebb764f5a
Beelal-Afzal <beelalchaudhary@gmail.com>
2026-03-15 04:17:46 +0500

    "fix: add missing imports")

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
 
-// Create an API that serves zero functions
+// Create an API that serves the functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
